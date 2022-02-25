2df5e7a5f92a08aa710f67edd11691fbb5f380da
Younggun Park <ygpark0922@gmail.com>
2022-02-25 23:53:27 +0900

    change members

--- pages/member.tsx
+++ pages/member.tsx
@@ -9,11 +9,12 @@ const Home: NextPage = () => {
                 <Grid item xs={12} md={4}>
                     <Card>
                         <CardContent>
-                            <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>문상원 | 2018</Typography>
+                            <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>2018, 팀장</Typography>
                             <Typography variant="h5" component="div">
+                                문상원
                             </Typography>
                             <Typography variant="body2">
-                                팀장
+
                             </Typography>
                         </CardContent>
                     </Card>
@@ -21,11 +22,12 @@ const Home: NextPage = () => {
                 <Grid item xs={12} md={4}>
                     <Card>
                         <CardContent>
-                            <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>박영건 | 2018</Typography>
+                            <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>2018, 기획담당</Typography>
                             <Typography variant="h5" component="div">
+                                박영건 | jadru
                             </Typography>
                             <Typography variant="body2">
-                                기획담당
+
                             </Typography>
                         </CardContent>
                     </Card>
@@ -33,11 +35,12 @@ const Home: NextPage = () => {
                 <Grid item xs={12} md={4}>
                     <Card>
                         <CardContent>
-                            <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>박성훈 | 2018</Typography>
+                            <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>2018, 학술담당</Typography>
                             <Typography variant="h5" component="div">
+                                박성훈 | shonn
                             </Typography>
                             <Typography variant="body2">
-                                교육담당
+
                             </Typography>
                         </CardContent>
                     </Card>
@@ -45,11 +48,25 @@ const Home: NextPage = () => {
                 <Grid item xs={12} md={4}>
                     <Card>
                         <CardContent>
-                            <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>최진서 | 2018</Typography>
+                            <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>2018, 사무담당</Typography>
                             <Typography variant="h5" component="div">
+                                최진서
                             </Typography>
                             <Typography variant="body2">
-                                사무담당
+
+                            </Typography>
+                        </CardContent>
+                    </Card>
+                </Grid>
+                <Grid item xs={12} md={4}>
+                    <Card>
+                        <CardContent>
+                            <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>2021, 정회원</Typography>
+                            <Typography variant="h5" component="div">
+                                신종웅
+                            </Typography>
+                            <Typography variant="body2">
+
                             </Typography>
                         </CardContent>
                     </Card>
