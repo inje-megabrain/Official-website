59511bc8c6ec0e9cc495461c7c6af653b3d85451
Younggun Park <ygpark0922@gmail.com>
2022-03-02 18:40:17 +0900

    add new member

--- pages/member.tsx
+++ pages/member.tsx
@@ -24,7 +24,7 @@ const Home: NextPage = () => {
                         <CardContent>
                             <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>2018, 기획담당</Typography>
                             <Typography variant="h5" component="div">
-                                박영건 | jadru
+                                박영건
                             </Typography>
                             <Typography variant="body2">
 
@@ -37,7 +37,7 @@ const Home: NextPage = () => {
                         <CardContent>
                             <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>2018, 학술담당</Typography>
                             <Typography variant="h5" component="div">
-                                박성훈 | shonn
+                                박성훈
                             </Typography>
                             <Typography variant="body2">
 
@@ -51,6 +51,19 @@ const Home: NextPage = () => {
                             <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>2018, 사무담당</Typography>
                             <Typography variant="h5" component="div">
                                 최진서
+                            </Typography>
+                            <Typography variant="body2">
+
+                            </Typography>
+                        </CardContent>
+                    </Card>
+                </Grid>
+                <Grid item xs={12} md={4}>
+                    <Card>
+                        <CardContent>
+                            <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>2018, 정회원</Typography>
+                            <Typography variant="h5" component="div">
+                                성병석
                             </Typography>
                             <Typography variant="body2">
 
