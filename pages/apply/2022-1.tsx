f964466cd9c3d91a7e68c969a4925b4f2a006c3c
Younggun Park <ygpark0922@gmail.com>
2022-03-04 13:33:44 +0900

    make new member apply avaliable

--- pages/apply/2022-1.tsx
+++ pages/apply/2022-1.tsx
@@ -9,11 +9,11 @@ const Home: NextPage = () => {
                 대상 : 2022년 1학기 기준 컴퓨터공학과 1학년 신학생<br />
                 기간 : 2022년 3월 4일(금) ~ 2022년 3월 11일(금)<br />
                 형태 : 준회원<br />
-                면접 : 추가 일정은 추후 개인 공지
+                면접 : 2022년 3월 14일(월) ~ 2022년 3월 18일(금) 중
             </Typography>
             <br />
-            <Button variant="contained" disabled={true} disableElevation href="https://notionforms.io/forms/2022-4" size="large">
-                모집 기간에 오픈 예정
+            <Button variant="contained" disableElevation href="https://notionforms.io/forms/2022-4" size="large">
+                모집 지원서 작성하기
             </Button>
         </>
     );
---
+++ pages/database.tsx
@@ -0,0 +1,106 @@
+import type { NextPage } from "next";
+import { Client } from "@notionhq/client"
+import {Card, CardContent, Grid, Typography} from "@mui/material";
+
+const Home: NextPage = () => {
+    return (
+        <>
+            <h1>회원 소개</h1>
+            <Grid container spacing={2}>
+                <Grid item xs={12} md={4}>
+                    <Card>
+                        <CardContent>
+                            <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>2018, 팀장</Typography>
+                            <Typography variant="h5" component="div">
+                                문상원
+                            </Typography>
+                            <Typography variant="body2">
+
+                            </Typography>
+                        </CardContent>
+                    </Card>
+                </Grid>
+                <Grid item xs={12} md={4}>
+                    <Card>
+                        <CardContent>
+                            <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>2018, 기획담당</Typography>
+                            <Typography variant="h5" component="div">
+                                박영건
+                            </Typography>
+                            <Typography variant="body2">
+
+                            </Typography>
+                        </CardContent>
+                    </Card>
+                </Grid>
+                <Grid item xs={12} md={4}>
+                    <Card>
+                        <CardContent>
+                            <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>2018, 기술담당</Typography>
+                            <Typography variant="h5" component="div">
+                                박성훈
+                            </Typography>
+                            <Typography variant="body2">
+
+                            </Typography>
+                        </CardContent>
+                    </Card>
+                </Grid>
+                <Grid item xs={12} md={4}>
+                    <Card>
+                        <CardContent>
+                            <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>2018, 인사담당</Typography>
+                            <Typography variant="h5" component="div">
+                                최진서
+                            </Typography>
+                            <Typography variant="body2">
+
+                            </Typography>
+                        </CardContent>
+                    </Card>
+                </Grid>
+                <Grid item xs={12} md={4}>
+                    <Card>
+                        <CardContent>
+                            <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>2021, 정회원</Typography>
+                            <Typography variant="h5" component="div">
+                                신종웅
+                            </Typography>
+                            <Typography variant="body2">
+
+                            </Typography>
+                        </CardContent>
+                    </Card>
+                </Grid>
+                <Grid item xs={12} md={4}>
+                    <Card>
+                        <CardContent>
+                            <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>2018, 정회원</Typography>
+                            <Typography variant="h5" component="div">
+                                성병석
+                            </Typography>
+                            <Typography variant="body2">
+
+                            </Typography>
+                        </CardContent>
+                    </Card>
+                </Grid>
+                <Grid item xs={12} md={4}>
+                    <Card>
+                        <CardContent>
+                            <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>2018, 정회원</Typography>
+                            <Typography variant="h5" component="div">
+                                최원석
+                            </Typography>
+                            <Typography variant="body2">
+
+                            </Typography>
+                        </CardContent>
+                    </Card>
+                </Grid>
+            </Grid>
+        </>
+    );
+};
+
+export default Home;
