356b0d13f412f492c4411c494cad26093b1982ca
Younggun Park <ygpark0922@gmail.com>
2022-03-04 18:37:14 +0900

    change sentence

--- pages/database.tsx
+++ pages/database.tsx
@@ -3,102 +3,37 @@ import { Client } from "@notionhq/client"
 import {Card, CardContent, Grid, Typography} from "@mui/material";
 
 const Home: NextPage = () => {
+
+    const notion = new Client({ auth: process.env.NOTION_KEY })
+
+    const databaseId = process.env.NOTION_DATABASE_ID
+
+    async function addItem(text) {
+        try {
+            const response = await notion.pages.create({
+                parent: { database_id: databaseId },
+                properties: {
+                    title: {
+                        title:[
+                            {
+                                "text": {
+                                    "content": text
+                                }
+                            }
+                        ]
+                    }
+                },
+            })
+            console.log(response)
+            console.log("Success! Entry added.")
+        } catch (error) {
+            console.error(error.body)
+        }
+    }
     return (
         <>
             <h1>회원 소개</h1>
-            <Grid container spacing={2}>
-                <Grid item xs={12} md={4}>
-                    <Card>
-                        <CardContent>
-                            <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>2018, 팀장</Typography>
-                            <Typography variant="h5" component="div">
-                                문상원
-                            </Typography>
-                            <Typography variant="body2">
 
-                            </Typography>
-                        </CardContent>
-                    </Card>
-                </Grid>
-                <Grid item xs={12} md={4}>
-                    <Card>
-                        <CardContent>
-                            <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>2018, 기획담당</Typography>
-                            <Typography variant="h5" component="div">
-                                박영건
-                            </Typography>
-                            <Typography variant="body2">
-
-                            </Typography>
-                        </CardContent>
-                    </Card>
-                </Grid>
-                <Grid item xs={12} md={4}>
-                    <Card>
-                        <CardContent>
-                            <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>2018, 기술담당</Typography>
-                            <Typography variant="h5" component="div">
-                                박성훈
-                            </Typography>
-                            <Typography variant="body2">
-
-                            </Typography>
-                        </CardContent>
-                    </Card>
-                </Grid>
-                <Grid item xs={12} md={4}>
-                    <Card>
-                        <CardContent>
-                            <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>2018, 인사담당</Typography>
-                            <Typography variant="h5" component="div">
-                                최진서
-                            </Typography>
-                            <Typography variant="body2">
-
-                            </Typography>
-                        </CardContent>
-                    </Card>
-                </Grid>
-                <Grid item xs={12} md={4}>
-                    <Card>
-                        <CardContent>
-                            <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>2021, 정회원</Typography>
-                            <Typography variant="h5" component="div">
-                                신종웅
-                            </Typography>
-                            <Typography variant="body2">
-
-                            </Typography>
-                        </CardContent>
-                    </Card>
-                </Grid>
-                <Grid item xs={12} md={4}>
-                    <Card>
-                        <CardContent>
-                            <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>2018, 정회원</Typography>
-                            <Typography variant="h5" component="div">
-                                성병석
-                            </Typography>
-                            <Typography variant="body2">
-
-                            </Typography>
-                        </CardContent>
-                    </Card>
-                </Grid>
-                <Grid item xs={12} md={4}>
-                    <Card>
-                        <CardContent>
-                            <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>2018, 정회원</Typography>
-                            <Typography variant="h5" component="div">
-                                최원석
-                            </Typography>
-                            <Typography variant="body2">
-
-                            </Typography>
-                        </CardContent>
-                    </Card>
-                </Grid>
-            </Grid>
         </>
     );
 };
