dbd8655e3e1f928d5edc4645192be6668e55a61e
Younggun Park <ygpark0922@gmail.com>
2022-03-07 00:08:10 +0900

    add github icon for members

--- pages/member.tsx
+++ pages/member.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from "next";
-import {Card, CardContent, Grid, Typography} from "@mui/material";
+import {Button, Card, CardContent, Grid, IconButton, Typography} from "@mui/material";
+import GitHubIcon from '@mui/icons-material/GitHub';
 
 const Home: NextPage = () => {
     return (
@@ -11,7 +12,10 @@ const Home: NextPage = () => {
                         <CardContent>
                             <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>2018, 팀장</Typography>
                             <Typography variant="h5" component="div">
-                                문상원
+                                문상원 <IconButton color="primary" aria-label="github profile" size="large"
+                                                onClick={() => window.open('https://github.com/MoonSangWon', '_blank')}>
+                                <GitHubIcon />
+                                </IconButton>
                             </Typography>
                             <Typography variant="body2">
 
@@ -24,7 +28,10 @@ const Home: NextPage = () => {
                         <CardContent>
                             <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>2018, 기획담당</Typography>
                             <Typography variant="h5" component="div">
-                                박영건
+                                박영건 <IconButton color="primary" aria-label="github profile" size="large"
+                                                onClick={() => window.open('https://github.com/jadru', '_blank')}>
+                                <GitHubIcon />
+                                </IconButton>
                             </Typography>
                             <Typography variant="body2">
 
@@ -37,7 +44,10 @@ const Home: NextPage = () => {
                         <CardContent>
                             <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>2018, 기술담당</Typography>
                             <Typography variant="h5" component="div">
-                                박성훈
+                                박성훈 <IconButton color="primary" aria-label="github profile" size="large"
+                                                onClick={() => window.open('https://github.com/Hoon9901', '_blank')}>
+                                <GitHubIcon />
+                            </IconButton>
                             </Typography>
                             <Typography variant="body2">
 
@@ -50,7 +60,10 @@ const Home: NextPage = () => {
                         <CardContent>
                             <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>2018, 인사담당</Typography>
                             <Typography variant="h5" component="div">
-                                최진서
+                                최진서 <IconButton color="primary" aria-label="github profile" disabled = {true}size="large"
+                                                onClick={() => window.open('https://github.com/', '_blank')}>
+                                <GitHubIcon />
+                            </IconButton>
                             </Typography>
                             <Typography variant="body2">
 
@@ -63,7 +76,10 @@ const Home: NextPage = () => {
                         <CardContent>
                             <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>2021, 정회원</Typography>
                             <Typography variant="h5" component="div">
-                                신종웅
+                                신종웅 <IconButton color="primary" aria-label="github profile" size="large"
+                                                onClick={() => window.open('https://github.com/ShinJongUng', '_blank')}>
+                                <GitHubIcon />
+                            </IconButton>
                             </Typography>
                             <Typography variant="body2">
 
@@ -76,7 +92,10 @@ const Home: NextPage = () => {
                         <CardContent>
                             <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>2018, 정회원</Typography>
                             <Typography variant="h5" component="div">
-                                성병석
+                                성병석 <IconButton color="primary" aria-label="github profile" size="large"
+                                                onClick={() => window.open('https://github.com/sbs1621', '_blank')}>
+                                <GitHubIcon />
+                            </IconButton>
                             </Typography>
                             <Typography variant="body2">
 
@@ -89,7 +108,10 @@ const Home: NextPage = () => {
                         <CardContent>
                             <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>2018, 정회원</Typography>
                             <Typography variant="h5" component="div">
-                                최원석
+                                최원석 <IconButton color="primary" aria-label="github profile" disabled = {true}size="large"
+                                                onClick={() => window.open('https://github.com/', '_blank')}>
+                                <GitHubIcon />
+                            </IconButton>
                             </Typography>
                             <Typography variant="body2">
 
